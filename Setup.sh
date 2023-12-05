f5edf61b201cc605ceaa91e82f461bfcba5bc788
Shreya Ramakrishnan <hi22sr@iiphh.org>
2023-12-05 21:13:28 +0530

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -2,7 +2,3 @@
 mkdir -p ~/.streamlit/
 echo "[server]\nheadless = true\nport = $PORT\n" > ~/.streamlit/config.toml
 pip install --upgrade streamlit
-pip install streamlit==1.15.0
-pip install pandas==1.3.3
-pip install matplotlib==3.4.3
-
